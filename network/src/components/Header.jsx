a821d6b3f0c68ce9114bcfecdf63eac35fba8083
Dmitry <82338843+Dmitry2310@users.noreply.github.com>
2021-08-06 16:45:21 +0300

    Commit_2
    Rendering the appearance of the application

---
+++ network/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src ='https://pixy.org/src/11/thumbs350/119344.jpg'></img>
+        <span>Friends Finder</span>
+        </header>);
+}
+
+export default Header;
---
+++ network/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+          <div className='bgDescription'>
+            <div className='avatar'></div>
+            <div className='description'>ghghghghghgh</div>
+          </div>
+          <div>
+            my posts
+           <div>new post</div>
+          </div>
+          <div>post1</div>
+      </div>);
+}
+
+export default Profile;
